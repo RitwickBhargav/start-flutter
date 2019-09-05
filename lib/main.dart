46ccd0cabfc5d260af36f1a42d36268588e3ea35
Shubham Goswami <sgshubham98@gmail.com>
2019-09-05 16:14:26 +0530

    pack.. import

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:english_words/english_words.dart' as prefix0;
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp()); // Use arrow for one liner function/method
 
@@ -7,17 +9,20 @@ class MyApp extends StatelessWidget { //Uses Stateless widget makes the app itse
   @override
   Widget build(BuildContext context) { // Widget main job to provide build method
     // build method describes how to display the widgets
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Hello App',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: Scaffold( // Scaffold widget provides default app/title bar and body property
-          appBar: AppBar(
+      home: Scaffold( 
+        // Scaffold widget provides default app/title bar and body property
+           appBar: AppBar(
             title: Text('Welcome to Flutter'),
-          ),
+           ),
           body: Center(
-            child: Text('Keep Fluttering'),
+            // child: Text('Keep Fluttering'),
+            child: Text(wordPair.asPascalCase),
           ),
       ),
       );
